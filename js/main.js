1535af38cce7b623ad7c83a04095d3900b1a2556
Mavlon4658 <mavlonbe4658>
2024-05-18 11:59:18 +0500

    index page is ready

---
+++ js/main.js
@@ -0,0 +1,49 @@
+let homeSwp = new Swiper('.home .swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    effect: 'fade',
+    speed: 500,
+    loop: true,
+    navigation: {
+        nextEl: '.home .swiper_btn__next',
+        prevEl: '.home .swiper_btn__prev',
+    }
+})
+
+let selectModalParentSwp = new Swiper('.select_modal .swiper-parent', {
+    direction: "vertical",
+    slidesPerView: 'auto',
+    speed: 500,
+    spaceBetween: 60,
+    // loop: true,
+    centeredSlides: true,
+    mousewheel: true,
+    // freeMode: true,
+    initialSlide: 4,
+})
+let selectModalChildSwps = document.querySelectorAll('.select_modal .swiper-child');
+if (selectModalChildSwps.length) {
+    selectModalChildSwps.forEach(el => {
+        let swp = new Swiper(el, {
+            direction: "vertical",
+            slidesPerView: 'auto',
+            speed: 500,
+            spaceBetween: 60,
+            // loop: true,
+            centeredSlides: true,
+            mousewheel: true,
+            // freeMode: true,
+        })
+    })
+}
+
+let selectModalOpen = document.querySelectorAll('.select_modal__open'),
+    selectModal = document.querySelector('.select_modal');
+
+if (selectModalOpen.length) {
+    selectModalOpen.forEach(btn => {
+        btn.onclick = () => {
+            selectModal.classList.add('active');
+        }
+    })
+}
